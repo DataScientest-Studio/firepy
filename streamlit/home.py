ecc67414000b094cc3a4761bcc3cff1f7469c1f6
icecore013 <31386060+icecore013@users.noreply.github.com>
2022-03-18 13:52:56 -0400

    Update home.py

--- streamlit/home.py
+++ streamlit/home.py
@@ -5,4 +5,7 @@ import inspect                 #+Deployment
 def app():
 
     currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))) 
-    st.image(os.path.join(currentdir, 'ressources/banner.jpg'))
+   
+
+    #st.image(os.path.join(currentdir, 'ressources/banner.jpg'))
+    st.image(os.path.join(currentdir, 'ressources/banner2.jpg'))
