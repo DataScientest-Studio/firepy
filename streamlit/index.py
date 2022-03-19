bdfceff7345627856fd3efa6ffd6260118231d51
FF <f.faupin@free.fr>
2022-03-19 18:59:58 +0100

    conclusion

--- streamlit/index.py
+++ streamlit/index.py
@@ -1,37 +1,39 @@
-import streamlit as st 
+import streamlit as st
 
-import os                      #+Deployment
-import inspect                 #+Deployment
+import os  # +Deployment
+import inspect  # +Deployment
 import home
 import dataset
 import prediction
 import about
-#import conclusion
+import conclusion
 import home
 import streamlit.components.v1 as components
 
-currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+currentdir = os.path.dirname(os.path.abspath(
+    inspect.getfile(inspect.currentframe())))
 
 logo = os.path.join(currentdir, 'ressources/logo_fire.png')
-PAGE_CONFIG = {"page_title":"Firepy","page_icon": logo,"layout":"wide"}
+PAGE_CONFIG = {"page_title": "Firepy", "page_icon": logo, "layout": "wide"}
 st.set_page_config(**PAGE_CONFIG)
 
 MENU_ = {
-    "Home" : home,
-    "Dataset" : dataset,
-    "Prediction" : prediction,
-    "About" : about,
-
-#    Conclusion" : conclusion
+    "Home": home,
+    "Dataset": dataset,
+    "Prediction": prediction,
+    "About": about,
+    "Conclusion": conclusion
 }
 
 st.sidebar.title('Menu')
-selection_page = st.sidebar.radio("",list(MENU_.keys()))
+selection_page = st.sidebar.radio("", list(MENU_.keys()))
 
 st.sidebar.title('Credits')
-            
-st.sidebar.write("[Emmanuelle CANO ](https://www.linkedin.com/in/emmanuelle-cano-4b845940/)")
-st.sidebar.write("[François FAUPIN](https://www.linkedin.com/in/francois-faupin/)")
+
+st.sidebar.write(
+    "[Emmanuelle CANO ](https://www.linkedin.com/in/emmanuelle-cano-4b845940/)")
+st.sidebar.write(
+    "[François FAUPIN](https://www.linkedin.com/in/francois-faupin/)")
 st.sidebar.write("[Thomas GOSSART](https://www.linkedin.com/in/gossartt/)")
 st.sidebar.write("Mentor :")
 st.sidebar.write("[Pierre ADEIKALAM](https://www.linkedin.com/in/data-jesus/)")
@@ -40,4 +42,3 @@ st.sidebar.title('Github')
 st.sidebar.write("[FirePy](https://github.com/DataScientest-Studio/firepy)")
 page = MENU_[selection_page]
 page.app()
-
